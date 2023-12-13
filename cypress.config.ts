05d6835952a2a4a91dc76f270ab08032c8377846
Mohamed Saeed <42535636+MohamedSci@users.noreply.github.com>
2023-12-13 22:13:15 +0200

    Json & CSV -> Working //BBD, Cucumber  Excel -> Not Working

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "cypress";
 import { Task } from "listr2/dist/lib/task";
-
+const cucumber = require("cypress-cucumber-preprocessor").default
 export default defineConfig({
   e2e: {
     experimentalStudio:true,
     baseUrl: "https://demo.nopcommerce.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('file:preprocessor',cucumber())
     },
+    specPattern:"cypress/e2e/BDD_Registration/*.feature"
   },
 });
