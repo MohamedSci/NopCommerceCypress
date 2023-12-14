1ab841d168adb59bc4ed6bd7fbe39a6f648d83d3
Mohamed Saeed <42535636+MohamedSci@users.noreply.github.com>
2023-12-14 20:57:37 +0200

    Interception & Cookie

--- cypress/e2e/normalRegisterationTest.cy.ts
+++ cypress/e2e/normalRegisterationTest.cy.ts
@@ -1,3 +1,4 @@
+import cypress from "cypress";
 
 /***
  * 1) Put your json file in fixture folder
@@ -5,6 +6,17 @@
  * 3) call .then(data) to assign this data to public variable to enable you to use it in your tests
  * 4) Use for loop to access the each data entity from  every row in your csv data file
  */
+/***
+ * 1) From dev tools console Got to Network and click the api og Post Registeration
+ * 2) before your action on UI make an interception and assign it to aliase
+ * 3) Perform The Action on UI
+ * 4)  add wait command to wait for the Interception
+ * 5) call get(@Interception) commannd and then()
+ * 6) print in the console the interception [object] 
+ * 7) Make validation on the interception's response or the request
+ * 8) Store The Auth token cookie, etc to process environment or cypress environment
+ */
+let testCookie
 describe('template spec', () => {
   before('passes', () => {
     cy.visit('/')
@@ -19,25 +31,38 @@ describe('template spec', () => {
       // Input valid data into all required fields registeration form
       cy.get("#FirstName").type("Moha11")
       cy.get("#LastName").type("lastname11")
-      cy.get("#Email").type("tewq124@ttt.com")
+      cy.get("#Email").type("tewq222@ttt.com")
       cy.get("#Password").type("password")
       cy.get("#ConfirmPassword").type("password")
       // Submit to registeration form
       cy.get('#register-button').click();
       // ... Perform your test logic ...
       cy.wait('@post_Reg')
-
+      // Validate the data in the Interception
       cy.get('@post_Reg').then((interception)=>{
-      console.log('interception ---------')
       console.log(interception)
       expect( interception["state"] ).eq("Complete")
       expect( interception["response"]["statusCode"]).eq(302)
-      console.log('********************** interception **********************')
-})
-    // Check the Submission is succeeded
-      cy.contains("Your registration completed").should("to.be.exist")
+                                                })
+      // Check the Submission is succeeded
+      cy.contains("Your registration completed").should("to.be.exist")//A confirmation doaloge in UI is Displayed    
+      // Assign the authentication cookie to environment variable 
+      cy.getCookie(".Nop.Authentication").then(cookie=>{
+        testCookie=cookie.value
+        console.log("-----------cookie.value ---"+cookie.value)
+        process.env.nopAuth = cookie.value
+        Cypress.env("nopAuth",cookie.value)
+      })
+
+      console.log("-----------Cypress.env Cookie ---"+Cypress.env("nopAuth"))
+      console.log("-----------process.env.nopAuth Cookie ---"+process.env.nopAuth)
+      expect(process.env.nopAuth).eq(testCookie)
+      expect(Cypress.env("nopAuth")).eq(testCookie)
+      console.log("************ Finished ********************")
+    })
       
-    });
+
+
     
     
 
