8126ce39abbb9eba5cacd584511bfd04e0f67815
Mohamed Saeed <42535636+MohamedSci@users.noreply.github.com>
2023-12-17 20:09:58 +0200

    BDD is Working

--- cypress/e2e/bddReg.feature
+++ cypress/e2e/bddReg.feature
@@ -1,12 +1,11 @@
 Feature: User Registeration First time
- @focus
-Scenario: A new user inputs new valid data for Registeration
-Given The user in the Home page 
-When The new User wants to navigate to Registeration screen
-And The User inputs valid First Name
-And The User inputs valid Last Name
-And The user inputs an email was not registered before
-And The user Inputs valid Password
-And The User Inputs the inputed password again in The confirmation text field
-And The User clicks Submit button
-Then The Registeration is succeeded
+  Scenario: A new user inputs new valid data for Registeration
+	Given The user in the Home page 
+	When The new User wants to navigate to Registeration screen
+	And The User inputs valid First Name
+	And The User inputs valid Last Name
+	And The user inputs an email was not registered before
+	And The user Inputs valid Password
+	And The User Inputs the inputed password again in The confirmation text field
+	And The User clicks Submit button
+	Then The Registeration is succeeded	
---
+++ cypress/support/step_definitions/loginPageSteps.js
@@ -0,0 +1,35 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
+
+// Scenario : A new user inputs new valid data for Registeration
+
+
+Given("The user in the Home page",()=>{
+    cy.visit('/')
+})
+When("The new User wants to navigate to Registeration screen",()=>{
+        // Navigate to registeration form
+        cy.get('.ico-register').click();
+})
+When("The User inputs valid First Name",()=>{
+    cy.get("#FirstName").type("aqw")
+
+})
+When("The User inputs valid Last Name",()=>{
+    cy.get("#LastName").type("wqa")
+})
+When("The user inputs an email was not registered before",()=>{
+    cy.get("#Email").type("ata.email165@test.com")
+})
+When("The user Inputs valid Password",()=>{
+    cy.get("#Password").type("12345678")
+})
+When("The User Inputs the inputed password again in The confirmation text field",()=>{
+    cy.get("#ConfirmPassword").type("12345678")
+})
+When("The User clicks Submit button",()=>{
+    cy.get('#register-button').click();
+})
+Then("The Registeration is succeeded",()=>{
+    cy.contains("Your registration completed").should("to.be.exist")
+})
+
